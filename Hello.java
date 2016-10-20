94ba328a2f46e60bad2e627586df4637c6ea8848
769875452 <769875452@qq.com>
2016-10-20 21:04:09 +0800

    修改

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class  Hello
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("Hello World23333!");
+		System.out.println("Hello World233333!");
 	}
 }
